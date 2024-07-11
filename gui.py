cd32481fc932309f1012c10279912e9c7a91d7e0
Sriharsha <sriharshaprjvh@gmail.com>
2024-07-11 13:33:00 +0530

    Created a Executable File. First installed the package pyinstaller after in the terminal a cmd line : pyinstall --onefile --windowed --clean file.py

--- gui.py
+++ gui.py
@@ -1,13 +1,18 @@
 import FreeSimpleGUI as G
 from readAndWriteFiles import file_read, file_write
 import time
+import os
 # Search in google pysimplegui themes go to images and select.
 G.theme('NeutralBlue')
 
-time_ = G.Text('',key = 't')
+if not os.path.exists('store.txt'):
+    with open('store.txt', 'w') as file:
+        pass
+
+time_ = G.Text('', key='t')
 label = G.Text('Enter the Data')
 InText = G.InputText(tooltip='Enter the Text', key='text')
-add_button = G.Button('Add')
+add_button = G.Button(key='Add', image_source='add.png', size=3)
 
 list_box = G.Listbox(values=file_read(), key='list', enable_events=True, size=[45, 15])
 edit_button = G.Button('Edit')
@@ -16,18 +21,16 @@ delete_button = G.Button('Delete')
 
 exit_button = G.Button('Exit')
 
-w = G.Window('My first Gui Code', layout=[[time_],[label, InText, add_button],
+w = G.Window('My first Gui Code', layout=[[time_], [label, InText, add_button],
                                           [list_box, edit_button, delete_button],
-                                          [exit_button]] ,
-                                font=('Helvetica', 13) )
+                                          [exit_button]], font=('Helvetica', 13))
 
 
 while True:
 
-
     event, values = w.read(timeout=200)
     # w.read() will return a tuple as (Add,{0: 'hi'}).Here event=Add and values={0: 'hi'}
-    w['t'].update(value = time.strftime("%b %d, %Y %H:%M:%S"))
+    w['t'].update(value=time.strftime("%b %d, %Y %H:%M:%S"))
     # if we need to know how the code is running remove the below comments
     # print(event)
     # print(values)
@@ -54,7 +57,7 @@ while True:
                 print(data_list)
                 w['list'].update(values=data_list)
             except IndexError:
-                G.popup('Select the item First!',font = ('Helvetica',10))
+                G.popup('Select the item First!', font=('Helvetica', 10))
 
         case 'Delete':
             try:
